748199580d4e581dd17ca371d612a08976063b62
SeoYoungEui <59830648+SeoYoungEui@users.noreply.github.com>
2020-03-21 17:44:08 +0900

    Revert "text 편집"
    This reverts commit c5ea5b3a9125455ad64a31e80d35ce7f605b1508.

--- OrderMatch/OrderMatch_frontend/src/components/Authentication/CheckStep2.js
+++ OrderMatch/OrderMatch_frontend/src/components/Authentication/CheckStep2.js
@@ -97,7 +97,7 @@ class CheckStep2 extends React.Component {
       passwordConfirmState: "",
       id: "",
       idState: "",
-      showPassword: false
+      showPassword: "false"
     };
   }
 
@@ -248,7 +248,7 @@ class CheckStep2 extends React.Component {
         this.setState({ idState: "error" });
       }
     }
-    return false;
+    return true;
   }
   render() {
     const { classes } = this.props;
